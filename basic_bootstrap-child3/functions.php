b086f84a22b746b6a796a35802385f044fc34bb5
reeganaga <rega.blank@gmail.com>
2016-10-06 13:59:52 +0700

    revisi option framework

--- basic_bootstrap-child3/functions.php
+++ basic_bootstrap-child3/functions.php
@@ -13,8 +13,6 @@
         }
     }
 
-define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/options-framework/' );
-require_once dirname( __FILE__ ) . '/options-framework/options-framework.php';
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
